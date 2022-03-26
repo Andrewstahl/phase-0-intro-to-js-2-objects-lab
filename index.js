452e7f775aefcd025d2aa810420904a0e671ee94
Andrew Stahl <andrewstahl96@gmail.com>
2022-03-26 12:36:02 -0400

    commit tree e5568cd7d5f754919e358303998cfa5117c94351

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+const employee = {
+    name: "Andrew",
+    streetAddress: "149 Ross Avenue",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+        ...employee, 
+        [key]: value,
+    };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const secondEmployee = {...employee};
+    delete secondEmployee[key];
+
+    return secondEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+
+    return employee;
+}
